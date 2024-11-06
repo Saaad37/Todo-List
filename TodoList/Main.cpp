1afa1bdb8412bf925d54acaffbfec3b9020b04e5
Saad <saadnore08@gmail.com>
2024-11-06 20:35:25 +0100

    Treating tasks case per case, adding a list of all the tasks and changing type of taskID to long instead of int to avoid data loss

--- TodoList/Main.cpp
+++ TodoList/Main.cpp
@@ -1,27 +1,62 @@
+#include <list>
 #include "raylib.h"
+
+using namespace std;
 
 #define MAX_INPUT_CHARS 48
 
 Rectangle textBox;
 char inputText[MAX_INPUT_CHARS + 1] = "\0";
 bool mouseOnButton = false;
+list<int> tasks; 
+bool isInputEmpty = true;
+
+bool searchFor(long taskID) {
+	for (int i : tasks) {
+		if (i == taskID) return true;
+	}
+	return false;
+}
+
+void resetInputText() {
+	if(!isInputEmpty){
+		for (int i = 0; i < MAX_INPUT_CHARS;i++) {
+			inputText[i] = '\0';
+		}
+	}
+}
 
 
 class Task {
 public:
 	bool Completed = false;
-	int TaskID = 0;
+	bool DoCreate = true;
+	long taskID = 0;
 	char taskText[MAX_INPUT_CHARS];
 
 	Task(int taskID) {
 		frame = { 10, 80.0f + taskID * 10, textBox.width, textBox.height };
-		this->TaskID;
+		this->taskID;
 	}
+
+
+	
+
+	void CreateText() {
+			while (DoCreate) {
+				for (int i = 0;i < MAX_INPUT_CHARS;i++) {
+					taskText[i] = inputText[i];
+				}
+				DoCreate = false;
+			}
+	}
+
 
 	void Draw()
 	{
 		DrawRectangleRec(frame, LIGHTGRAY);
 		DrawText(taskText, frame.x + 10, frame.y + 10, 20, BLACK);
+		resetInputText();
 	}
 private:
 	Rectangle frame;
@@ -44,9 +79,13 @@ int main() {
 	Rectangle submitTaskButton = { screenWidth - 120, 10, 105, 58 };
 	bool mouseOnText = false;
 
-	Task task = Task(0);
+	Task task1 = Task(0);
+	Task task2 = Task(1);
+	Task task3 = Task(2);
+	Task task4 = Task(3);
 
-	int numClicksSubmit = 0;
+
+	long numClicksSubmit = 0;
 	int framesCounter = 0;
 
 	SetTargetFPS(60);
@@ -76,6 +115,7 @@ int main() {
 
 			while (key > 0) {
 				if (key > 31 && key < 125 && letterCount < MAX_INPUT_CHARS) {
+					isInputEmpty = false;
 					inputText[letterCount] = (char)key;
 					inputText[letterCount + 1] = '\0';
 					letterCount++;
@@ -86,6 +126,7 @@ int main() {
 				letterCount--;
 				if (letterCount < 0) {
 					letterCount = 0;
+					isInputEmpty = true;
 				}
 				inputText[letterCount] = '\0';
 			}
@@ -98,12 +139,6 @@ int main() {
 		else framesCounter = 0;
 
 		if (mouseOnButton && IsMouseButtonDown(MOUSE_BUTTON_LEFT) && inputText[0] != '\0') numClicksSubmit++;
-		if(numClicksSubmit == task.TaskID + 1) {
-			for (int i = 0;i < MAX_INPUT_CHARS;i++) {
-				task.taskText[i] = inputText[i];
-			}
-		}
-
 		
 		BeginDrawing();
 		
@@ -156,11 +191,38 @@ int main() {
 			DrawText("Submit Task", submitTaskButton.x + submitTaskButton.width / 2 - 31, submitTaskButton.height / 2 + 5, 10, BLACK);
 			SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 		}
-		if (numClicksSubmit >= 1) {
-			task.Draw();
-		}
-		else DrawText("No Tasks to display", 150,screenHeight/2, 40, MAROON);
-	
+
+		switch (numClicksSubmit) {
+		case 0:
+			DrawText("No Tasks to display", 150, screenHeight / 2, 40, MAROON);
+			break;
+		case 1:
+			task1.CreateText();
+			task1.Draw();
+			if(searchFor(task1.taskID)) break;
+			else tasks.push_back(task1.taskID);
+			break;
+		case 2:
+			task2.CreateText();
+			task2.Draw();
+			if (searchFor(task2.taskID)) break;
+			else tasks.push_back(task2.taskID);
+			break;
+		case 3:
+			task3.CreateText();
+			task3.Draw();
+			if (searchFor(task3.taskID)) break;
+			else tasks.push_back(task3.taskID);
+			break;
+		case 4:
+			task4.CreateText();
+			task4.Draw();
+			if (searchFor(task4.taskID)) break;
+			else tasks.push_back(task4.taskID);
+			break;
+		default:
+			break;
+		}	
 		EndDrawing();
 	}
 
